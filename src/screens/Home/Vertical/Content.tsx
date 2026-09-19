9b476c35de0ac88219378f1a2a83ef02e169b15e
Alger <alger@music.app>
2026-09-19 23:16:17 +0800

    feat(ui): Salt Player风格 - 改回侧边抽屉导航+汉堡菜单+列表按钮播放条

--- src/screens/Home/Vertical/Content.tsx
+++ src/screens/Home/Vertical/Content.tsx
@@ -1,6 +1,8 @@
+import { useRef } from 'react'
+import DrawerLayoutFixed from '@/components/common/DrawerLayoutFixed'
 import Header from './Header'
+import DrawerNav from './DrawerNav'
 import Main from './Main'
-import { View } from 'react-native'
 import { createStyle } from '@/utils/tools'
 
 const styles = createStyle({
@@ -10,11 +12,21 @@ const styles = createStyle({
 })
 
 const Content = () => {
+  const drawerRef = useRef<DrawerLayoutFixed>(null)
+
   return (
-    <View style={styles.container}>
-      <Header />
-      <Main />
-    </View>
+    <DrawerLayoutFixed
+      ref={drawerRef}
+      drawerWidth={280}
+      drawerPosition="left"
+      drawerBackgroundColor="rgba(0,0,0,0)"
+      renderNavigationView={() => <DrawerNav onShowDrawer={() => drawerRef.current?.closeDrawer()} />}
+    >
+      <>
+        <Header onShowDrawer={() => drawerRef.current?.openDrawer()} />
+        <Main />
+      </>
+    </DrawerLayoutFixed>
   )
 }
 
--- src/screens/Home/Vertical/index.tsx
+++ src/screens/Home/Vertical/index.tsx
@@ -1,13 +1,11 @@
 import Content from './Content'
 import PlayerBar from '@/components/player/PlayerBar'
-import BottomTabBar from '@/components/common/BottomTabBar'
 
 export default () => {
   return (
     <>
       <Content />
       <PlayerBar isHome />
-      <BottomTabBar />
     </>
   )
 }
